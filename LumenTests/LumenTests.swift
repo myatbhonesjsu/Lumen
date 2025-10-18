06d9747338426e10b7a349e0c54d1432524140a5
myat555 <myat-555@hotmail.com>
2025-10-18 11:39:19 -0700

    Initial Commit

---
+++ LumenTests/LumenTests.swift
@@ -0,0 +1,17 @@
+//
+//  LumenTests.swift
+//  LumenTests
+//
+//  Created by Myat Bhone San on 10/18/25.
+//
+
+import Testing
+@testable import Lumen
+
+struct LumenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
